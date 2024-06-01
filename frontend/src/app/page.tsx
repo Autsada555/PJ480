ee28bcc56613b12c0d6ca1b082a0ade1397bef67
Autsada555 <autsadaheha@gmail.com>
2024-06-01 14:14:26 +0700

    firstcommit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <main className="flex-col items-center justify-between ">
+      อันนี้คือหน้าแรกกก เอาไว้ทำหน้า HOME คลิกไปหน้าอื่นๆกับทดลอง
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+      <Button variant="outline">Button</Button>
+
+    </main>
+  );
+}
